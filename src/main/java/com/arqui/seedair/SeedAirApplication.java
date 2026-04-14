60ca4344dcbfd0c3003d64c9ba9269dfdbaaeb23
rodrigo190410 <rodrigopachasfelix@gmail.com>
2026-04-13 23:04:23 -0500

    feat: estructura inicial de Spring Boot y Postgres

---
+++ src/main/java/com/arqui/seedair/SeedAirApplication.java
@@ -0,0 +1,13 @@
+package com.arqui.seedair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedAirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedAirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arqui/seedair/SeedAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arqui.seedair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedAirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
